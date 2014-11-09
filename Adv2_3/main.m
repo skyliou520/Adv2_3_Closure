6d91cb5165419b80c5313fcc154b5dd7c879830d
skyliou520 <skyliou520@gmail.com>
2014-11-09 17:38:29 +0800

    finish sort

--- Adv2_3/main.m
+++ Adv2_3/main.m
@@ -7,16 +7,20 @@
 //
 
 #import <Foundation/Foundation.h>
+#import "Asset.h"
 typedef void (^EchoBlock)(void);
 typedef float(^TotalPrice)(int,int);
 int counter = 0;
 int main(int argc, const char * argv[]) {
     @autoreleasepool {
-        NSDictionary* phoneBook =
-        @{@"Mark":@"1234567",@"John":@"1111111"};
-        [phoneBook enumerateKeysAndObjectsUsingBlock:^(id key, id obj, BOOL *stop) {
-            NSLog(@"key=%@,value=%@",key,obj);
-        }];
+        
+        Asset* asset = [[Asset alloc]initWithName:@"James"USD:2000 EURO:3000];
+        NSLog(@"asset dictionary is %@", asset.description);
+//        NSDictionary* phoneBook =
+//        @{@"Mark":@"1234567",@"John":@"1111111"};
+//        [phoneBook enumerateKeysAndObjectsUsingBlock:^(id key, id obj, BOOL *stop) {
+//            NSLog(@"key=%@,value=%@",key,obj);
+//        }];
     }
     return 0;
 }
