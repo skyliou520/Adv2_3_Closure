ec2b61a0ed48711480bff03e83dd13e2d5e78dd1
skyliou520 <skyliou520@gmail.com>
2014-11-09 16:51:04 +0800

    Block variable 1.when use global variable. it can change in block

--- Adv2_3/main.m
+++ Adv2_3/main.m
@@ -9,25 +9,14 @@
 #import <Foundation/Foundation.h>
 typedef void (^EchoBlock)(void);
 typedef float(^TotalPrice)(int,int);
+int counter = 0;
 int main(int argc, const char * argv[]) {
     @autoreleasepool {
-        // insert code here...
-        EchoBlock myBlock =^(void){NSLog(@"Hello Echo!");};
-        myBlock(); myBlock(); myBlock();
-        myBlock =^(void){NSLog(@"Refill");};
-        myBlock();
-        float discount = 0.9f;
-        TotalPrice total = ^(int unitPrice, int quantity){
-            return discount*unitPrice*quantity;
+        EchoBlock echoBlock = ^()
+        {
+            NSLog(@"counter = %d",counter++);
         };
-        NSLog(@"buy iPhone, total:%.1f",total(30000,8));
-        NSLog(@"buy iPad, total:%.1f", total(25000,4));
-        discount = 0.5f;
-        total = ^(int unitPrice, int quantity){
-            return discount*unitPrice*quantity;
-        };
-        NSLog(@"[1111] buy iPhone, total:%.1f",total(30000,8));
-        NSLog(@"[1111] buy iPad, total:%.1f", total(25000,4));
+        echoBlock();echoBlock();echoBlock();
     }
     return 0;
 }
