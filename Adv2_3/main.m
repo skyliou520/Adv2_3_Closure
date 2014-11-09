9e8034613d6a9bf8a039fc661c9b4b25037eb654
skyliou520 <skyliou520@gmail.com>
2014-11-09 16:28:54 +0800

    Initial Commit

---
+++ Adv2_3/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  Adv2_3
+//
+//  Created by pw's air on 2014/11/9.
+//  Copyright (c) 2014年 pw's air. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
